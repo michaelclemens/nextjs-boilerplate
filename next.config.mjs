96c415fc5c7c810055fed7b8bb782b0ac3bd961b
michaelclemens <michaelclemens8@hotmail.com>
2024-09-17 16:00:22 +0100

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  eslint: {
+    dirs: ['app', 'components', 'hooks', 'lib', 'test', 'types', 'util'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
